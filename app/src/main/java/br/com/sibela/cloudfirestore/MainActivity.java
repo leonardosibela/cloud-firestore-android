baf3010f33e7e56ee76869292640424265094486
leonardosibela <leonardo_sibela@hotmail.com>
2018-05-25 14:18:27 -0300

    Saving data to cloud firestore

--- app/src/main/java/br/com/sibela/cloudfirestore/MainActivity.java
+++ app/src/main/java/br/com/sibela/cloudfirestore/MainActivity.java
@@ -1,10 +1,20 @@
 package br.com.sibela.cloudfirestore;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,11 +22,15 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String QUOTE_KEY = "quote";
+    private static final String AUTHOR_KEY = "author";
+    private DocumentReference docRef = FirebaseFirestore.getInstance().document("sampleData/inpiration");
+
     @BindView(R.id.quote_input)
     EditText quoteInput;
 
-    @BindView(R.id.person_name_input)
-    EditText personName;
+    @BindView(R.id.author_name_input)
+    EditText authorName;
 
     @BindView(R.id.quote_text)
     TextView quoteText;
@@ -30,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.save_button)
     public void save(View view) {
+        String quote = quoteInput.getText().toString();
+        String authorName = this.authorName.getText().toString();
 
+        Map<String, Object> dataToSave = new HashMap<>();
+        dataToSave.put(QUOTE_KEY, quote);
+        dataToSave.put(AUTHOR_KEY, authorName);
+
+        docRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(MainActivity.this, "Salvo com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Falha ao salvar!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
